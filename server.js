e9de51b84fee9f3d55a142c650ee7887b7d261ad
Christopher <chrisformey@yahoo.com>
2019-07-24 21:18:55 -0500

    setup skeleton for server.js HTTP methods

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+app.get('/posts',(req,res) => {
+
+})
+
+
+app.get('/posts:id',(req,res) => {
+
+})
+
+
+app.post('/posts',(req,res) => {
+
+})
+
+
+app.put('/posts:id', (req,res) => {
+
+})
+
+
+
+app.delete('/posts:id', (req,res) => {
+    
+})
