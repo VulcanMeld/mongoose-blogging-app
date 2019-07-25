7ab4da8522ce5c722875f90b43bc7c423a15d398
Christopher <chrisformey@yahoo.com>
2019-07-25 18:19:17 -0500

    push to atlas and heroku

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 "use strict";
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost:27017/mongoose-blog-app";
+  process.env.DATABASE_URL || "mongodb+srv://admin:moneyGetter24>@my-first-atlas-db-ujaod.mongodb.net/mongoose-blogging-app?retryWrites=true&w=majority"
+  ;
 exports.PORT = process.env.PORT || 8080;
